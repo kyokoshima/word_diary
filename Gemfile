79d8caf2d6b568199d6c4c4a626a7f1aa343e732
kyokoshima <dev.kyoko@gmail.com>
2016-08-09 02:42:00 +0000

    change react_Webpack_rails to react_on_rails

--- Gemfile
+++ Gemfile
@@ -50,4 +50,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # react
-gem 'react_webpack_rails'
+gem 'react_on_rails'
--- word_diary/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  get 'react_examples/component', to: 'react_examples#component', as: :component
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
