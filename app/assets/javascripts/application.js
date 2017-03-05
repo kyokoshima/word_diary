ad1a21ba435bbc2cc749eac36f44184720362bfc
kyokoshima <dev.kyoko@gmail.com>
2017-03-06 05:10:02 +0800

    enable moment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require jquery.remotipart
+//= require moment
 //= require barba
 //= require 'greensock/TweenMax'
 //= require 'greensock/plugins/CSSPlugin'
