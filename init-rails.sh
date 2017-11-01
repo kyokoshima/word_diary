57f06263619b8afda1e9405e333c810047ccb31e
IK_O <d.scovery0911@gmail.com>
2017-11-01 22:11:07 +0900

    migrated initializing db command to shell script

---
+++ init-rails.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "command rails db:create db:migrate"
+rails db:create db:migrate
+
+echo "command bundle exec rails s -p 3000 -b '0.0.0.0"
+bundle exec rails s -p 3000 -b '0.0.0.0'
