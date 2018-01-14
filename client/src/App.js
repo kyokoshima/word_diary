20231ab70b5e174c49269f199d331e50182c1f1d
kyokoshima <dev.kyoko@gmail.com>
2018-01-14 17:26:58 +0900

    Modify top screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { TextField, RaisedButton } from 'material-ui';
 import './App.css';
+import { RadioButton } from 'material-ui/RadioButton';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">A Word Diary</h1>
+          </header>
+          <div>
+            <div>
+             <TextField hintText="Email" />
+            </div>
+            <div>
+              <TextField hintText="Password" type="password" />
+            </div>
+            <div>
+              <RaisedButton label="Sign in" primary={true} />
+            </div>
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
